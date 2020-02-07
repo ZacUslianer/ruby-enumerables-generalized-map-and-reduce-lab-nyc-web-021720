2057e9caa6c64abccc4c4b1a8db9a3146d6ba1e9
Zac Uslianer <zacuslianer@gmail.com>
2020-02-07 23:42:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,5 +10,17 @@ counter += 1
  new_array
 end
 
-def reduce(array)
+def reduce(array, starting_value = nil)
+  if starting_value
+      sum = starting_value
+      i = 0 
+    else
+      sum = array[0]
+      i = 1 
+    end 
+    while i < array.length 
+    sum = yield(sum, array[i])  
+    i += 1
+  end
+sum
 end
